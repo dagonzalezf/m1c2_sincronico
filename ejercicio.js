7274bd878a91a6adcf3ec09e2489a5932aea4f84
David González <david.andres.gonzalez.84@gmail.com>
2022-05-30 18:32:00 -0400

    Ejercicio en clase M1/C2

---
+++ ejercicio.js
@@ -0,0 +1,6 @@
+const nombre = prompt ('Hola! ¿cuál es tu nombre?');
+let saludo = 'Hola' + nombre;
+const edad = prompt ('¿Qué edad tienes?');
+saludo = 'Hola ' + nombre + " de " + edad + ' años';
+alert (saludo);
+console.log (saludo)
